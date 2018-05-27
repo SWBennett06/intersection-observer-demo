7f834f40030ebdf8e11a700901d9adcdfeabc5f2
Sean Bennett <swbennett06@gmail.com>
2018-05-27 12:28:54 -0500

    REF: Updated callback var name DEL: Removed commented out log

--- src/observerDemo.ts
+++ src/observerDemo.ts
@@ -4,13 +4,11 @@ export default function runIODemo() {
   const observables = document.querySelectorAll('section');
   
   const obsOptions: IntersectionObserverInit = {
-    threshold: [0]
+    threshold: 0
   };
   
-  const obsHandler: IntersectionObserverCallback = (entries, observer) => {  
+  const obsCallback: IntersectionObserverCallback = (entries, observer) => {  
     entries.forEach((entry, index) => {
-      // console.count();
-      
       const element = entry.target as HTMLElement;
       const imgElement = element.getElementsByTagName('img')[0];
       
@@ -26,7 +24,7 @@ export default function runIODemo() {
     });
   };
   
-  const observer = new IntersectionObserver(obsHandler, obsOptions);
+  const observer = new IntersectionObserver(obsCallback, obsOptions);
   
   observables.forEach(observable => {
     observer.observe(observable);
